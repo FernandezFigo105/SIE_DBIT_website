1c41e9d59d74b030e786ccf68dc662279011a1de
Jess John <jessjohn1539@gmail.com>
2023-07-31 19:59:49 +0530

    initial commit

---
+++ tree.js
@@ -0,0 +1,12 @@
+// var geojsonFeature = {
+//     "type": "Feature",
+//     "properties": {
+//         "name": "Coors Field",
+//         "amenity": "Baseball Stadium",
+//         "popupContent": "This is where the Rockies play!"
+//     },
+//     "geometry": {
+//         "type": "Point",
+//         "coordinates": [19.08170, 72.88911]
+//     }
+// };
